efa3fbc2854eae803a91f150b83efb4ec05426d3
Paul Bolden <paul.bolden@digipathways.org>
2020-07-10 14:22:55 -0400

    Add project files.

---
+++ SMSDesktopUWP/Views/GuardianMasterDetailDetailControl.xaml.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+using SMSDesktopUWP.Core.Models;
+
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+
+namespace SMSDesktopUWP.Views
+{
+    public sealed partial class GuardianMasterDetailDetailControl : UserControl
+    {
+        public SampleOrder MasterMenuItem
+        {
+            get { return GetValue(MasterMenuItemProperty) as SampleOrder; }
+            set { SetValue(MasterMenuItemProperty, value); }
+        }
+
+        public static readonly DependencyProperty MasterMenuItemProperty = DependencyProperty.Register("MasterMenuItem", typeof(SampleOrder), typeof(GuardianMasterDetailDetailControl), new PropertyMetadata(null, OnMasterMenuItemPropertyChanged));
+
+        public GuardianMasterDetailDetailControl()
+        {
+            InitializeComponent();
+        }
+
+        private static void OnMasterMenuItemPropertyChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
+        {
+            var control = d as GuardianMasterDetailDetailControl;
+            control.ForegroundElement.ChangeView(0, 0, 1);
+        }
+    }
+}
---
+++ SMSDesktopUWP/Views/SponsorMasterDetailDetailControl.xaml.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+using SMSDesktopUWP.Core.Models;
+
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+
+namespace SMSDesktopUWP.Views
+{
+    public sealed partial class SponsorMasterDetailDetailControl : UserControl
+    {
+        public SampleOrder MasterMenuItem
+        {
+            get { return GetValue(MasterMenuItemProperty) as SampleOrder; }
+            set { SetValue(MasterMenuItemProperty, value); }
+        }
+
+        public static readonly DependencyProperty MasterMenuItemProperty = DependencyProperty.Register("MasterMenuItem", typeof(SampleOrder), typeof(SponsorMasterDetailDetailControl), new PropertyMetadata(null, OnMasterMenuItemPropertyChanged));
+
+        public SponsorMasterDetailDetailControl()
+        {
+            InitializeComponent();
+        }
+
+        private static void OnMasterMenuItemPropertyChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
+        {
+            var control = d as SponsorMasterDetailDetailControl;
+            control.ForegroundElement.ChangeView(0, 0, 1);
+        }
+    }
+}
