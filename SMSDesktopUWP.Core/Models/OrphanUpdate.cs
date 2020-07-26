a3ee3781e293d33b70c7e5276ad736e4245a8362
davewevans <60859872+davewevans@users.noreply.github.com>
2020-07-26 16:14:03 -0400

    Added WebApiService

---
+++ SMSDesktopUWP.Core/Models/OrphanUpdate.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SMSDesktopUWP.Core.Models
+{
+    public class OrphanUpdate
+    {
+        public string FirstName { get; set; } = string.Empty;
+
+        public string MiddleName { get; set; } = string.Empty;
+
+        public string LastName { get; set; } = string.Empty;
+
+        public string Gender { get; set; }
+
+        public DateTime DateOfBirth { get; set; }
+
+        public string LCMStatus { get; set; }
+
+        public string ProfileNumber { get; set; }
+
+        public DateTime EntryDate { get; set; }
+
+        public int? GuardianID { get; set; }
+    }
+}
